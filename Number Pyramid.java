00e4a9269000e50e4c805b3497fdd7e879a43746
Maharajasivam M <120156771+Maharaja2004@users.noreply.github.com>
2025-04-26 21:16:32 +0530

    Create Number Pyramid.java

---
+++ Number Pyramid.java
@@ -0,0 +1,18 @@
+package basicprograms;
+
+public class NumberPyramid {
+
+	public static void main(String[] args) {
+		int n=4;
+		for(int i=1;i<=n;i++) {
+			for(int j=n-i;j>0;j--) {
+				System.out.print(" ");
+			}
+			for(int j=1;j<=(2*i-1);j++) {
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+	}
+
+}
